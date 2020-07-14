8a20b2f818a6399683a2a68c0f8a7e30a0c1d7d2
aminwetzel <68148524+aminwetzel@users.noreply.github.com>
2020-07-14 12:59:08 +0100

    Add files via upload

---
+++ TF Model Creation/export_model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h
